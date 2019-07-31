790fd43248269fd0853568cab50a33c0331abd2f
keven <keven0632@gmail.com>
2019-07-31 16:08:20 +0800

    first commit

---
+++ list.py
@@ -0,0 +1,7 @@
+# 进行列表操作
+a_list = ['a', 'b', 'c']
+print(a_list)
+a_list.append('dfdf')
+print(a_list)
+a_list.remove('a')
+print(a_list)
---
+++ test.py
@@ -0,0 +1,4 @@
+# 这是一个 python 程序
+chinese = "猴鸡狗猪鼠牛虎兔龙蛇马羊"
+currentYear = 2016
+print(chinese[currentYear % 12])
